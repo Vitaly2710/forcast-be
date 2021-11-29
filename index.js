027a5e28f6af9cf567d9c28c2986ce75d1882439
unknown <cheastyakov3@gmail.com>
2021-11-29 15:04:02 +0300

    port

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ app.get('/weather', async (req, res) => {
   }
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
