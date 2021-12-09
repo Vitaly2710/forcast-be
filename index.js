68ffdbb5b71df8467ee6acb467ef449216bfe7ee
Vitaly2710 <cheastyakov3@gmail.com>
2021-12-09 14:53:54 +0300

    refresh

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
 
-
 setInterval(function () {
   http.get("https://forecast-be.herokuapp.com");
 }, 300000);
